b1644b2815090e4a9e166b0e104b5b200ea2be8e
riccardosl <riccardostoppani@gmail.com>
2019-11-30 18:57:25 +0100

    upload of folder with OpneNLP and first examples of static chatbot

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject chatbot "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[nightlight/lein-nightlight "2.4.1"]
+            [org.clojure/core.unify "0.5.7"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+		[clojure-opennlp "0.5.0"]]
+  :repl-options {:init-ns chatbot.core})
