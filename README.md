42a9a4d5fa154fdac94fbcfe2f67f4b333d1b4e3
riccardosl <riccardostoppani@gmail.com>
2019-12-08 23:22:07 +0100

    second version with conditions

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# clojure-chatbot
-Preparing a Clojure ChatBot for having useful info about parks!
+# Park Chatbot
+
+This small application is developed to give a sample of the Prague authorities of the introduction of a chatbot with user interaction.
+The demo wants to present basic function of asking the name of the user, asking different questions about the specific requirements for a public park in Prague.
+The chatbot is collecting feedback from the user and offering information about a public park offering a link to the online maps or a small description to reach it.
+## Installation
+
+The chatbot is written in Clojure, a functional programming language running on Java Virtual Machine.
+Depending on your operation system different steps are necessary to obtain the following components:
+
+
+Java Development Kit (JDK) version 11 or newer
+
+[Leiningen](https://leiningen.org/)
+
+A command line (CLI)
+
+
+## Usage
+
+```bash
+
+git clone https://github.com/riccardosl/clojure-chatbot.git  # Clone the github repository
+cd /clojure-chatbot # Browse the project folder
+lein run # Run the chatbot and start interaction
+
+```
+
+## Authors
+[Riccardo](https://github.com/riccardosl) &
+[Jan](https://github.com/jandvorak-dot)
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/chatbot/core.clj
+++ src/chatbot/core.clj
@@ -2,26 +2,68 @@
   (:require [clojure.string :as str]
             [clojure.pprint :as p]))
 
-
+(use 'clojure.java.browse)
 
 
 
 (defn read-input []
-  (loop [state :hello]  
+  (loop [state :hello]
       (let [input (read-line)]
-          (when-not (= ":done" input) 
-           (cond 
-             (= state :hello) (do 
-                                (println (str "Nice to meet you " input " Are you interested in a park"))
-                                (recur :park-interested?)) 
-             (= state :park-interested?)  
-             (cond (= input "yes") (recur :park-interested-yes)
-                   (= input "no") (println "Bye bye")
+          (when-not (= ":done" input)
+           (cond
+             (= state :hello) (do
+                                (println (str "Nice to meet you " input "! Are you interested in a park? Type 'yes' or 'no'"))
+                                (recur :park-interested?))
+             (= state :park-interested?)
+             (cond (= input "yes") (do
+                                     (println (str "Would you a parking space for a car? Type 'yes' or 'no'"))
+                                     (recur :park-interested-yes))
+                   (= input "no") (println "I can't help sorry. I'm just a park bot. Bye")
                     :else (do (println "Please answer yes or no")
                             (recur state)))
+             (= state :park-interested-yes)
+             (cond (= input "yes") (do
+                                     (println (str "Would you like to have food? Type 'yes' or 'no'"))
+                                     (recur :park-food-yes))
+                   (= input "no") (do
+                                    (println (str "Would you like to ride a bike? Type 'yes' or 'no'"))
+                                    (recur :park-bike-yes))
+                    :else (do (println "Please answer yes or no")
+                            (recur state)))
+             (= state :park-food-yes)
+             (cond (= input "yes") (do
+                                     (println (str "I suggest Betramka park. Can I open the link to the map? Type 'betramka' or 'no'"))
+                                     (recur :park-map-yes))
+                   (= input "no") (println "Pity they had Svickova on the menu today. Would you like to skate instead? Type 'yes' or 'no'")
+                   :else (do (println "Please answer betramka or no")
+                           (recur state)))
+             (= state :park-bike-yes)
+             (cond (= input "yes") (do
+                                     (println (str "Would you like to have food? Type 'yes' or 'no'"))
+                                     (recur :park-food-yes))
+                   (= input "no") (do
+                                    (println (str "Would you like to have food? Type 'yes' or 'no'"))
+                                    (recur :park-food-yes))
+                   :else (do (println "Please answer yes or no")
+                           (recur state)))
+             (= state :park-map-yes)
+             (cond (= input "betramka") (do (browse-url "https://en.mapy.cz/s/funetamute"))
+                   (= input "ladronka") (do (browse-url "https://en.mapy.cz/s/pakezonala"))
+                   (= input "no") (do
+                                    (println (str "Not a problem, my suggestion to reach it is Plzenska street or tram stop 'Betramka'")))
+                   :else (do (println "Please answer yes or no")
+                                         (recur state)))
+             (= state :park-skate-yes)
+             (cond (= input "yes") (do (println (str "I suggest Ladronka park. Can I open the link to the map? Type 'yes' or 'no'"))
+                                       (recur :park-map-yes))
+                   (= input "no") (do
+                                     (println (str "Not a problem, it also nice to walk")))
+             :else (do (println "Please answer yes or no")(recur state)))
              :else
              (do (println "Unknown state" state)
                (recur :hello)))))))
+
+
 
 (def test-phrase
   [["hello" "hello how can I help?"]
@@ -46,7 +88,7 @@
                                firststep)))
         "please say it again"))
 
-(def test-phrase2  
+(def test-phrase2
   [[#"I'm near (.*)" "Let me search a park near "]
    [#"hello (.*)" "nice to meet you "]
    [#"bye (.*)" "see you soon "]])
@@ -66,20 +108,6 @@
 
 (defn -main
   [& args]
-  (println "Hello, what is your name?")
-  (read-input))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  (println "Hello, I'm a chatbot. What is your name?")
+  (read-input)
+  (response-test2 "bye clojure"))
